1fad21902178893570760c5ae6f2bfed01377f0f
Mark Pometun <someone@ux.com>
2020-07-07 00:10:51 +0300

    app done

--- js/config.js
+++ js/config.js
@@ -16,16 +16,19 @@ export const months = [
 export const sidebarCities = [
   {
     city: "New York",
+    id: 5128581,
     img:
       "https://images.unsplash.com/photo-1518235506717-e1ed3306a89b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
   },
   {
     city: "Paris",
+    id: 2988507,
     img:
       "https://images.unsplash.com/photo-1502602898657-3e91760cbb34?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1052&q=80",
   },
   {
     city: "London",
+    id: 2643743,
     img:
       "https://images.unsplash.com/photo-1505761671935-60b3a7427bad?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
   },
--- js/script.js
+++ js/script.js
@@ -3,21 +3,16 @@ import { months, sidebarCities } from "./config.js";
 const WEATHER_API = "https://api.openweathermap.org/data/2.5/";
 const API_KEY = "cba9b27e4c6cd316c764d468b4ee756e";
 
-const CITIES_API =
-  "https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json";
-
 const LOCATION_API =
   "https://api.bigdatacloud.net/data/reverse-geocode-client?";
 
 const input = document.querySelector('[name="city"]');
-const list = document.querySelector(".list");
 const city = document.querySelector(".result-section");
 const reload = document.querySelector(".reload");
 const form = document.querySelector(".form");
-const day = document.querySelector(".date");
+const date = document.querySelector(".date");
 const sidebar = document.querySelector(".sidebar");
 const sidebarBtn = document.querySelector(".sidebar-btn");
-const cities = [];
 
 async function getCityWeather(query) {
   const res = await fetch(
@@ -27,6 +22,13 @@ async function getCityWeather(query) {
 
   city.innerHTML = displayCityWeather(data);
   form.reset();
+}
+
+function showDate() {
+  const now = new Date();
+  const day = now.getDate();
+  const month = months[now.getMonth()];
+  date.textContent = `${day < 10 ? "0" + day : day} ${month}`;
 }
 
 function useImg(icon) {
@@ -72,7 +74,6 @@ function removeCityCard() {
 
 function reloadPage(e) {
   e.preventDefault();
-  list.textContent = "";
   removeCityCard();
   form.reset();
 }
@@ -92,40 +93,38 @@ function submitSearch(e) {
   getCityWeather(input.value);
 }
 
-reload.addEventListener("click", reloadPage);
-form.addEventListener("submit", submitSearch);
-
 async function openSidebar() {
   sidebar.classList.toggle("open");
   if (sidebar.classList.contains("open")) {
     const html = await Promise.all(
-      sidebarCities.map(async ({ city, img }) => {
+      sidebarCities.map(async ({ city, img, id }) => {
         const res = await fetch(
           `${WEATHER_API}weather?q=${city}&appid=${API_KEY}&units=metric`
         );
         const data = await res.json();
-        return generateWeather(data, img);
+        return generateWeather(data, img, id);
       })
     );
     sidebar.innerHTML = html.join("");
   }
 }
 
-function generateWeather(city, img) {
+function generateWeather(city, img, id) {
   const { img: weatherImg, isNight } = useImg(city.weather[0].icon);
 
   return `
-    <div class="popular-city">
-      <img
-        class="city-img"
-        src="${img}"
-        alt="weather"
-      />
+    <div class="popular-city" data-id=${id}>
+      <div class="city-img"> 
+        <img
+          src="${img}"
+          alt="weather"
+        />
+      </div>
       <div class="info">
         <div class="title">
           <h3>${city.name}, ${city.sys.country}</h3>
           <p class="temperature">
-            ${Math.round(city.main.temp)}
+            ${Math.floor(city.main.temp)}
             <i class="fas fa-temperature-high icon-temp"></i>
           </p>
         </div>
@@ -138,21 +137,28 @@ function generateWeather(city, img) {
           alt="weather"
         />
         <div class="details">
-          <span>
-            <p>wind</p> 
-            ${city.wind.speed} m/s
-          </span>
-          <span>
-            <p>humidity</p>
-            ${city.main.humidity} %
-          </span>
+            <p>wind ${city.wind.speed} m/s</p> 
+            <p>humidity ${city.main.humidity} %</p>
         </div>
       </div>
     </div>
   `;
 }
 
+function closeSideBar(e) {
+  if (e.key === "Escape" && sidebar.classList.contains("open")) {
+    sidebar.classList.remove("open");
+  }
+}
+
+function openCityWeather(city) {
+  return window.open(`https://www.bbc.com/weather/${city}`, "_blank");
+}
+
+reload.addEventListener("click", reloadPage);
+form.addEventListener("submit", submitSearch);
 sidebarBtn.addEventListener("click", openSidebar);
+window.addEventListener("keyup", closeSideBar);
 
 navigator.geolocation.getCurrentPosition(
   (data) => {
@@ -163,3 +169,5 @@ navigator.geolocation.getCurrentPosition(
     alert("Type a city you want to find");
   }
 );
+
+showDate();
